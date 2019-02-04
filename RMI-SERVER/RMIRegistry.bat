274ad677b330c67a6f927093408b8b64a4a1257f
PedroAguirreArias01 <pedro.aguirre@uptc.edu.co>
2019-02-04 13:20:19 -0500

    Actializado

---
+++ RMI-SERVER/RMIRegistry.bat
@@ -0,0 +1,2 @@
+cd C:\Users\RA302\Desktop\RMI-JAVA\RMIServer\build\classes
+"C:\Program Files\Java\jdk1.8.0_161\bin\RMIRegistry.exe"
---
+++ RMI-SERVER/src/edu/uptc/sisdist/suma/InterfaceRemota.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uptc.sisdist.suma;
+import java.rmi.*;
+import java.io.Serializable;
+/**
+ * Interface remota con los métodos que se pueden llamar de forma remota
+ * @author Carlos Arenas
+ */
+public interface InterfaceRemota extends Remote {
+    public int suma (int a, int b) throws RemoteException; 
+    public int multiplicar(int a, int b) throws RemoteException;
+     public int resta (int a, int b) throws RemoteException; 
+    public int division(int a, int b) throws RemoteException;
+}
