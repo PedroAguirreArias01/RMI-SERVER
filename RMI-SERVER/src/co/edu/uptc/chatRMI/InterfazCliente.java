6e038dc790dff50a31c5e4617068474e66c92b3f
PedroAguirreArias01 <pedro.aguirre@uptc.edu.co>
2019-02-12 07:50:32 -0500

    Chat server

---
+++ RMI-SERVER/src/co/edu/uptc/chatRMI/InterfazCliente.java
@@ -0,0 +1,21 @@
+package co.edu.uptc.chatRMI;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author guillermo
+ */
+public interface InterfazCliente extends Remote {
+    
+    public void mostrarMensaje(String mensaje) throws RemoteException;
+    
+}
